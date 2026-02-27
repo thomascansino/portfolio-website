8a55f72674a43512b4834f83f7cb8f215117b532
thomas cansino <thomas.cansino@gmail.com>
2026-02-27 20:18:40 +0800

    Add javascript functionality to portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// your script here
+const testButton = document.getElementById("button-id")
+const firstName = document.getElementById("fname")
+const lastName = document.getElementById("lname")
+
+
+
+
+
+
+
+function buttonFunctionality() {
+    console.log("Button is clicked!")
+    alert("Button is clicked!")
+}
+
+
+
+
+
+
+
+
+
+
+
+testButton.addEventListener("click", buttonFunctionality)
